cf8617ed73a2536cbdafda149ec3642a929e25ef
BugraBalci <balcibugra4@gmail.com>
2023-03-08 16:18:38 +0300

    ikinci guncelleme

---
+++ CMakeFiles/UnitConverter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnitConverter.dir/main.c.o"
+  "CMakeFiles/UnitConverter.dir/main.c.o.d"
+  "CMakeFiles/UnitConverter.dir/yeni.c.o"
+  "CMakeFiles/UnitConverter.dir/yeni.c.o.d"
+  "UnitConverter"
+  "UnitConverter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UnitConverter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
